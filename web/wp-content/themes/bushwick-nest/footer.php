aaf347b4fe5d983c4d93473d41243c132c84a94f
Yoda <david.odie@digitalsix.co.uk>
2018-02-22 19:21:40 +0000

    theme - bushwick

---
+++ web/wp-content/themes/bushwick-nest/footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package Bushwick
+ */
+?>
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<?php do_action( 'bushwick_credits' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+	<?php wp_footer(); ?>
+
+</body>
+</html>
